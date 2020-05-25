6ba0b84552fb6b2cb5a718dce8c1f6fe080c8c2b
Sohame Rajput <rudranrajput@gmail.com>
2020-05-25 14:17:33 -0700

    this angular files

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls:['./post-create.component.css']
+})
+
+export class PostCreateComponent implements OnInit {
+  constructor() { }
+
+  newPost="No Content";
+
+  onAddPost(){
+    this.newPost='the user\'s post';
+  }
+
+  ngOnInit() { }
+}
