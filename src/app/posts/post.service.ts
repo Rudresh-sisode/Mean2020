078252a39e1d18e120ccd9625923fc105fd20e72
Sohame Rajput <rudranrajput@gmail.com>
2020-05-27 15:46:32 -0700

    additional activity added

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,15 +1,20 @@
 import { Post } from './post.model';
 import { Injectable } from '@angular/core';
-import { Subject}from 'rxjs'
+import { Subject}from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({providedIn:'root'})
 export class PostsService {
 
   private posts:Post[]=[];
   private postsUpdated = new Subject<Post[]>();
-
+constructor(private http:HttpClient){}
   getPosts(){
-    return [...this.posts];
+    this.http.get<{message:string,posts:Post[]}>('http://localhost:3000/api/posts')
+    .subscribe((postsData)=>{
+      this.posts = postsData.posts;
+      this.postsUpdated.next([...this.posts]);
+    })
     //spread operator
   }
   getPostUpdateListener(){
@@ -17,7 +22,7 @@ export class PostsService {
   }
 
   addPost(title:string,content:string){
-    const post:Post={title:title,content:content};
+    const post:Post={id:null,title:title,content:content};
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
